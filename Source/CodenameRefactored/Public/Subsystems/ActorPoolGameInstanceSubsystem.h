aa94fce280e2bcd8e041b8f3b95ba1fea386ebae
Erdeni Arsalanov <erdeni2001@yandex.ru>
2025-07-07 23:40:36 +0800

    Adding Base ActorSubsystem class in CPP
    also fixing git issues

---
+++ Source/CodenameRefactored/Public/Subsystems/ActorPoolGameInstanceSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "ActorPoolGameInstanceSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CODENAMEREFACTORED_API UActorPoolGameInstanceSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
