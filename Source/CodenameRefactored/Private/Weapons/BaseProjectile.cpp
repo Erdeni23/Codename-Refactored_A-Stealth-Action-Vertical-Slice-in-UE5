f572b9d9f9e1f0e91e4ab17cb8c5c60e6ac83b13
Erdeni Arsalanov <erdeni2001@yandex.ru>
2025-07-10 21:00:31 +0800

    Added Base Projectile Actor for Bullets, Rockets etc.

---
+++ Source/CodenameRefactored/Private/Weapons/BaseProjectile.cpp
@@ -0,0 +1,27 @@
+// Copyright (C) 2025 Erdeni Arsalanov. All Rights Reserved.
+
+
+#include "Weapons/BaseProjectile.h"
+
+// Sets default values
+ABaseProjectile::ABaseProjectile()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ABaseProjectile::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void ABaseProjectile::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
---
+++ Source/CodenameRefactored/Public/Weapons/BaseProjectile.h
@@ -0,0 +1,26 @@
+// Copyright (C) 2025 Erdeni Arsalanov. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "BaseProjectile.generated.h"
+
+UCLASS()
+class CODENAMEREFACTORED_API ABaseProjectile : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ABaseProjectile();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+};
